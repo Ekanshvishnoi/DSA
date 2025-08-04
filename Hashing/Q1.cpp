f2a4bf7bcea2317e330ab1aac55ad80e7bdf2996
Ekansh Vishnoi <ekanshvishnoi6@gmail.com>
2025-08-04 10:03:27 +0530

    Count Frequency

---
+++ Hashing/Q1.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std ;
+
+
+vector<vector<int>> countFreq(vector<int>& arr) {
+  unordered_map<long long, int > mpp ;//pre-computing
+  for(int num : arr){
+    mpp[ num]++ ;
+  }
+
+  //fetching
+  vector< vector<int> > result ;
+  for(auto& pair : mpp){
+    result.push_back( { pair.first, pair.second} ) ;
+  }
+  return result ;
+  }
+
+
+int main(){
+  
+  vector< int> arr;
+  int num ;
+
+  int n ;
+  cout << "Enter the size of array" << endl ;
+  cin >> n ;
+
+  for (int i =0 ; i<n ; i++){
+    cin >> num ;
+    arr.push_back(num) ;
+  }
+
+  vector<vector<int>> result = countFreq(arr) ;
+
+  for( auto& val :  result){
+    cout << val[0] << "occurs" << val[1] <<"times \n";
+  }
+  
+
+  return 0 ;
+}
