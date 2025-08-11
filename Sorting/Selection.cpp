aafabdc2f8036c765ebba461faa2dc7c18b45c6d
Ekansh Vishnoi <ekanshvishnoi6@gmail.com>
2025-08-11 09:54:32 +0530

    selection sort

--- Sorting/Selection.cpp
+++ Sorting/Selection.cpp
@@ -1,12 +1,39 @@
 #include<iostream> 
-
+#include <vector>
 using namespace std ; 
-int main (){
-  int k ;
-  for (int i =0 ; i< 6 ; i++){
-    cout << 1<< endl ;
 
-    cout << i << endl ;
-  }
+class Solution {
+  public:
+    void selectionSort(vector<int> &arr) {
+        int n = arr.size() ;
+        for(int i =0 ; i < n-1 ; i ++ ){
+            for (int j =i + 1  ; j < n ; j++ ){
+                if(arr[i] > arr[j]){
+                    swap(arr[i],arr[j]) ;
+                }   
+            }
+        }
+    }
+};
+
+int main (){
+  
+    int n ; 
+    cout << "Enter the no. of Elements of the array " << endl ;
+    cin >> n;
+    vector <int> nums ;
+    int num ;
+    cout << "Enter the Elements of the array " ;
+
+    for (int i = 0 ; i < n ; i ++ ){
+        cin >> num ;
+        nums.push_back(num) ;
+    }
+    Solution sol ;
+    sol.selectionSort(nums) ;
+    for ( auto& it : nums){
+      cout << it  << " " ;
+    }
+
   return 0 ;
 }
